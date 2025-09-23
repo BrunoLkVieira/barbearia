fb31f9c334599ec0c51d3e8be2f662719d34168d
Joao Santos <jmsdo1970@gmail.com>
2025-09-23 19:27:40 -0300

    refactor: ajuste plan-card

--- apps/system_plan/views.py
+++ apps/system_plan/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 def landing_page(request):
-     # return render(request, "system_plan/landing.html")
-    return render(request, "plan/plans.html")
+     return render(request, "system_plan/landing.html")
+    # return render(request, "schedule/agenda.html")
